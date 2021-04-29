5bb4fc373a5b3ca036ae814d9783ebed536e2ba8
Lucas Ribeiro <lucasrb04@gmail.com>
2021-04-28 21:05:38 -0300

    realizando requisito 5

---
+++ script.js
@@ -0,0 +1,41 @@
+// https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript
+function ballColor() {
+  const allBalls = document.querySelectorAll('.ball');
+  allBalls.forEach((ball) => {
+    const ballCollor = ball;
+    const r = Math.round(Math.random() * 255);
+    const g = Math.round(Math.random() * 255);
+    const b = Math.round(Math.random() * 255);
+    ballCollor.style.backgroundColor = `rgb(${r},${g},${b})`;
+  });
+}
+
+function colorToBeGess() {
+  const balls = document.querySelectorAll('.ball');
+  const randomPosition = Math.floor(Math.random() * 6);
+  const text = document.getElementById('rgb-color');
+  const ballColorToBeGuess = balls[randomPosition].style.backgroundColor;
+  const hidden = document.getElementById('hidden');
+  hidden.innerHTML = ballColorToBeGuess;
+  const reg = /\d+/g;
+  const textColor = `(${ballColorToBeGuess.match(reg)})`;
+  text.innerText = textColor;
+}
+
+const ballContainer = document.querySelector('#ball-container');
+
+ballContainer.addEventListener('click', (event) => {
+  const hidden = document.getElementById('hidden');
+  const answer = document.getElementById('answer');
+  const ballSelected = event.target.style.backgroundColor;
+  if (ballSelected === hidden.innerText) {
+    answer.innerText = 'Acertou!';
+  } else {
+    answer.innerText = 'Errou! Tente novamente!';
+  }
+});
+
+window.onload = function onload() {
+  ballColor();
+  colorToBeGess();
+};
